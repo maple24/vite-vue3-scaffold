7a8135e330b03ee9d47fb088601e8d884bf56020
ziu7wx <jin.zhu5@cn.bosch.com>
2023-05-05 12:46:23 +0800

    test: add example to toggle language in about.vue

--- src/views/about.vue
+++ src/views/about.vue
@@ -6,9 +6,9 @@ import HelloWorld from '@/components/HelloWorld.vue'
 const { t, availableLocales, locale } = useI18n();
 
 const toggleLocales = () => {
-  const locales = availableLocales;
   locale.value =
-    locales[(locales.indexOf(locale.value) + 1) % locales.length];
+    availableLocales[(availableLocales.indexOf(locale.value) + 1) % availableLocales.length];
+  console.log(locale.value);
 };
 
 const toggleDark = () => console.log("toggle theme");
@@ -38,12 +38,12 @@ setTimeout(() => {
         <li class="cursor-pointer text-2xl">
           <a href="#" @click="toggleLocales" class="footer-link text-cyan-700 hover:text-cyan-500"
             :title="t('toggle_language')">
-            <i class="i-ph-translate-bold" />
+            <i class="i-ph-translate-bold">{{ t('toggle_language') }}</i>
           </a>
         </li>
         <li class="cursor-pointer text-2xl">
           <a href="#" @click="toggleDark()" class="text-cyan-700 hover:text-cyan-500" :title="t('toggle_theme')">
-            <i i="ph-sun dark:ph-moon" />
+            <i i="ph-sun dark:ph-moon">{{ t('toggle_theme') }}</i>
           </a>
         </li>
         <li class="cursor-pointer text-2xl">
@@ -60,14 +60,3 @@ setTimeout(() => {
     </footer>
   </div>
 </template>
-
-<style>
-a,
-.footer-link {
-  @apply transition-all ease-out duration-100;
-}
-
-.footer-link {
-  opacity: 0.8;
-}
-</style>
--- src/views/draft.vue
+++ src/views/draft.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <p>draft page</p>
-        <a href="#" title="this is a title"></a>
+        <a href="#" title="this is a title">123</a>
     </div>
 </template>
 
