79f2cd3f24f6dc15eca6428ae63aa1086c3ba4bd
ziu7wx <jin.zhu5@cn.bosch.com>
2023-05-08 10:12:25 +0800

    feat: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,5 +19,6 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  }
+  },
+  test: {}
 })
